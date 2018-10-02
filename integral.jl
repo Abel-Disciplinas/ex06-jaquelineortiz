4d4352a3e3de0e5953107763eb6da664d049e7e1
jaquelineortiz <43250276+jaquelineortiz@users.noreply.github.com>
2018-10-01 22:43:40 -0300

    Update integral.jl

--- integral.jl
+++ integral.jl
@@ -2,17 +2,29 @@
 function simpsonrep(f, a, b; m = 101)
     m % 2 == 0 && (m += 1)
     h = (b - a) / (m - 1)
-    # Seu código de Simpson repetido
-    return 0.0
+    I1 = I2 = 0.0
+    xi = a + h
+    xj = a + 2 * h
+    for i = 2 : 2 :m - 1
+        I1 += f(xi)
+        xi += 2 * h
+    end
+    for k = 3 : 2 : m - 2
+        I2 += f(xj)
+        xj += 2 * h
+    end
+    return I = (h/3) * (f(a) + 4 * I1 + 2 * I2 + f(b))    
 end
 
 function simpsoneps(f, a, b, ϵ; M = 1.0)
-    # Calcule h, m e use a função simpsonrep
+   h = ((180 * ϵ) / ((b - a) * M)) ^ (0.25)
+   m = ceil(Int, (b - a)/ h + 1)
+   return simpsonrep(f, a, b, m)
 end
 
 # II
 function simpson(f, a, b)
-    # Simpson sem repetição
+    return ((b - a) / 6) * (f(a) + 4 * f((a + b) / 2) + f(b))
 end
 
 function simpson_adaptivo(f, a, b, ϵ)
@@ -21,5 +33,12 @@ function simpson_adaptivo(f, a, b, ϵ)
 end
 
 function simpson_adaptivo_recursivo(f, a, b, ϵ, I)
-    # Implementação de Simpson adaptativo
+    c = (a + b) / 2
+    esquerda = ((c - a) / 6) * (f(a) + 4 * f((a + c) / 2) + f(c)) #S(a,c)
+    direita = ((b - c) / 6) * (f(c) + 4 * f((c + b) / 2) + f(b)) #S(c,b)
+    if abs(I - esquerda - direita) <= 15 * ϵ
+        return esquerda + direita
+    else
+        return simpson_adaptivo_recursivo(f, a, c, ϵ/2, esquerda) + simpson_adaptivo_recursivo(f, c, b, ϵ/2, direita)
+    end
 end
